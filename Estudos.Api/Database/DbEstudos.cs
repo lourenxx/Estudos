92a25ead2dbff440a1427d06a72f376cd6388f8f
lourenxx <guilhermepexiloureco@gmail.com>
2025-12-23 14:16:42 -0300

    Conexão com banco db_Estudos e remoção de diretório Controller

---
+++ Estudos.Api/Database/DbEstudos.cs
@@ -0,0 +1,48 @@
+﻿using Estudos.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Estudos.Api.Database
+{
+    public class DbEstudos : DbContext
+    {
+        public DbEstudos(DbContextOptions<DbEstudos> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasSequence<int>("Seq_Usuarios", schema: "dbo")
+                .StartsAt(1)
+                .IncrementsBy(1);
+
+            builder.Entity<Usuarios>(entity =>
+            {
+                entity.ToTable("Usuarios");
+                
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                    .HasDefaultValueSql("NEXT VALUE FOR dbo.Seq_Usuarios");
+
+                entity.Property(e => e.Nome)
+                .HasMaxLength(100);
+
+                entity.Property(e => e.Email)
+                .HasMaxLength(100);
+
+                entity.Property(e => e.Senha)
+                .HasMaxLength(100);
+
+                entity.Property(e => e.DataCriacao)
+                .HasMaxLength(100);
+            });
+
+        }
+
+
+    }
+}
---
+++ Estudos.Api/Models/Usuarios.cs
@@ -0,0 +1,12 @@
+﻿namespace Estudos.Api.Models
+{
+    public class Usuarios
+    {
+
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public DateTime? DataCriacao { get; set; }
+    }
+}
